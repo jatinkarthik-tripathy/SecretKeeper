e008d6be25f8bd8160573978eeeb5870b3bc336c
Jatin <jatinkarthikt@gmail.com>
2021-05-05 11:48:26 +0530

    login UI done

--- secretkeeper/lib/screens/login.dart
+++ secretkeeper/lib/screens/login.dart
@@ -13,8 +13,10 @@ class Login extends StatelessWidget {
                 Expanded(
                   flex: 1,
                   child: Container(
+                    height: double.infinity,
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         Stack(
                           alignment: Alignment.center,
@@ -69,7 +71,7 @@ class Login extends StatelessWidget {
                               fontWeight: FontWeight.w600,
                             ),
                           ),
-                        )
+                        ),
                       ],
                     ),
                   ),
@@ -77,12 +79,148 @@ class Login extends StatelessWidget {
                 Expanded(
                   flex: 1,
                   child: Container(
+                    height: constraints.maxHeight,
                     decoration: BoxDecoration(
                       color: Theme.of(context).primaryColor,
                       borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(36),
                         bottomLeft: Radius.circular(36),
                       ),
+                    ),
+                    padding: EdgeInsets.all(constraints.maxWidth * 0.05),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "SecretKeeper",
+                          style: TextStyle(
+                            color: Theme.of(context).backgroundColor,
+                            fontSize: 48,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        SizedBox(
+                          height: constraints.maxHeight * 0.1,
+                        ),
+                        SizedBox(
+                          width: constraints.maxWidth * 0.35,
+                          child: Text(
+                            "Log in",
+                            style: TextStyle(
+                              color: Theme.of(context).backgroundColor,
+                              fontSize: 48,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: constraints.maxHeight * 0.03,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(36.0),
+                              ),
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: "Email",
+                            hintStyle: TextStyle(
+                              fontSize: 24,
+                            ),
+                          ),
+                          enableSuggestions: true,
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                        SizedBox(
+                          height: constraints.maxHeight * 0.015,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(36.0),
+                              ),
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: "Password",
+                            hintStyle: TextStyle(
+                              fontSize: 24,
+                            ),
+                          ),
+                          obscureText: true,
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                        SizedBox(
+                          height: constraints.maxHeight * 0.03,
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Forgot your Password ?",
+                            style: TextStyle(
+                              color: Theme.of(context).backgroundColor,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: constraints.maxHeight * 0.1,
+                        ),
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                              Theme.of(context).backgroundColor,
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: constraints.maxHeight * 0.05,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "or connect with",
+                              style: TextStyle(
+                                color: Theme.of(context).backgroundColor,
+                                fontSize: 24,
+                              ),
+                            ),
+                            SizedBox(width: 36),
+                            IconButton(
+                              iconSize: 48,
+                              icon: Container(
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white,
+                                ),
+                                padding: EdgeInsets.all(8),
+                                child: Image.asset('images/gLogo.png'),
+                              ),
+                              onPressed: () {},
+                            )
+                          ],
+                        ),
+                      ],
                     ),
                   ),
                 )
