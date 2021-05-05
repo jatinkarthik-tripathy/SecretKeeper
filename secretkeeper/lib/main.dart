7c30f9e0c35f275003eea4c05195c82b7737238c
Jatin <jatinkarthikt@gmail.com>
2021-05-05 10:37:34 +0530

    half of login page done

--- secretkeeper/lib/main.dart
+++ secretkeeper/lib/main.dart
@@ -12,7 +12,24 @@ class MyApp extends StatelessWidget {
       title: 'SecretKeeper',
       theme: ThemeData(
         primaryColor: Color(0xFF164F55),
+        accentColor: Color(0xFF164F55),
         backgroundColor: Colors.white,
+        fontFamily: 'Poppins',
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+              EdgeInsets.symmetric(
+                horizontal: 36,
+                vertical: 24,
+              ),
+            ),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(36.0),
+              ),
+            ),
+          ),
+        ),
       ),
       routes: <String, WidgetBuilder>{
         '/login': (BuildContext context) => Login(),
--- secretkeeper/lib/screens/login.dart
+++ secretkeeper/lib/screens/login.dart
@@ -3,6 +3,94 @@ import 'package:flutter/material.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).backgroundColor,
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            return Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Image.asset(
+                              'images/logoBG.png',
+                              height: 240,
+                            ),
+                            Image.asset(
+                              'images/logoFG.png',
+                              height: 200,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: constraints.maxHeight * 0.05,
+                        ),
+                        Text(
+                          "Hello, Friend!",
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 48,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        SizedBox(
+                          height: constraints.maxHeight * 0.05,
+                        ),
+                        Text(
+                          "tell me your secrets\nand i shall keep it safe for you",
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 36,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        SizedBox(
+                          height: constraints.maxHeight * 0.1,
+                        ),
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                              Theme.of(context).primaryColor,
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              color: Theme.of(context).backgroundColor,
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(36),
+                        bottomLeft: Radius.circular(36),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
