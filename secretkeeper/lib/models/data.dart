9ce01d5248a491733cc5206ef4c2446d9d325caa
Jatin <jatinkarthikt@gmail.com>
2021-05-07 16:41:20 +0530

    added listview to home page

---
+++ secretkeeper/lib/models/data.dart
@@ -0,0 +1,59 @@
+class PasswordData {
+  final int type;
+  final String title;
+  final String pwd;
+
+  PasswordData({
+    this.type,
+    this.title,
+    this.pwd,
+  });
+}
+
+class NoteData {
+  final int type;
+  final String title;
+  final String note;
+
+  NoteData({
+    this.type,
+    this.title,
+    this.note,
+  });
+}
+
+class CardData {
+  final int type;
+  final String title;
+  final String cardNumber;
+  final String expMonth;
+  final String expYear;
+  final String cvv;
+
+  CardData({
+    this.type,
+    this.title,
+    this.cardNumber,
+    this.expMonth,
+    this.expYear,
+    this.cvv,
+  });
+}
+
+class AccountData {
+  final int type;
+  final String title;
+  final String accountNumber;
+  final String loginPassword;
+  final String transactionPassword;
+  final String notes;
+
+  AccountData({
+    this.type,
+    this.title,
+    this.accountNumber,
+    this.loginPassword,
+    this.transactionPassword,
+    this.notes,
+  });
+}
--- secretkeeper/lib/screens/home.dart
+++ secretkeeper/lib/screens/home.dart
@@ -96,21 +96,27 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
     return Column(
       children: [
         Row(
+          mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            IconButton(
-              iconSize: 36,
-              icon: Icon(Icons.menu, color: Theme.of(context).primaryColor),
-              onPressed: () {
-                controller.forward();
-              },
-            ),
+            Breakpoints().isMobile(constraints)
+                ? IconButton(
+                    iconSize: 24,
+                    icon:
+                        Icon(Icons.menu, color: Theme.of(context).primaryColor),
+                    onPressed: () {
+                      controller.forward();
+                    },
+                  )
+                : SizedBox(
+                    width: 0,
+                  ),
             Expanded(
               child: Padding(
                 padding: EdgeInsets.symmetric(
                     horizontal: Breakpoints().isMobile(constraints) ? 8 : 32,
-                    vertical: 8),
+                    vertical: 16),
                 child: TextField(
                   decoration: InputDecoration(
                     border: OutlineInputBorder(
@@ -151,9 +157,9 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
             Breakpoints().isMobile(constraints)
                 ? CircleAvatar(
                     backgroundColor: Theme.of(context).primaryColor,
-                    radius: 28,
+                    radius: 24,
                     child: IconButton(
-                      iconSize: 36,
+                      iconSize: 24,
                       icon: Icon(Icons.add,
                           color: Theme.of(context).backgroundColor),
                       onPressed: () {},
@@ -183,7 +189,105 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                   ),
           ],
         ),
-        Expanded(child: Placeholder()),
+        Expanded(
+          child: ListView.builder(
+            physics: BouncingScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Container(
+                width: constraints.maxHeight * 0.05,
+                decoration: BoxDecoration(
+                  color: Color(0xFFE5E5E5),
+                  borderRadius: BorderRadius.circular(36.0),
+                ),
+                margin: EdgeInsets.symmetric(
+                    horizontal: constraints.maxWidth * 0.05, vertical: 16),
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Icon(
+                        Icons.security_outlined,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    Expanded(
+                      flex: Breakpoints().isMobile(constraints) ? 3 : 5,
+                      child: Breakpoints().isMobile(constraints)
+                          ? Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Title",
+                                  style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: plainTextSize,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                                Text(
+                                  "username",
+                                  style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: plainTextSize,
+                                  ),
+                                ),
+                              ],
+                            )
+                          : Row(
+                              children: [
+                                Expanded(
+                                  flex: 2,
+                                  child: Text(
+                                    "Title",
+                                    style: TextStyle(
+                                      color: Theme.of(context).primaryColor,
+                                      fontSize: plainTextSize,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 3,
+                                  child: Text(
+                                    "username",
+                                    style: TextStyle(
+                                      color: Theme.of(context).primaryColor,
+                                      fontSize: plainTextSize,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                            Theme.of(context).primaryColor,
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Show",
+                          style: TextStyle(
+                            color: Theme.of(context).backgroundColor,
+                            fontSize: plainTextSize,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+            itemCount: 25,
+          ),
+        )
       ],
     );
   }
